402b561c7cfe950713210a657f1b0aa08740d6a1
Mathias Voges <186237816+m-voges@users.noreply.github.com>
2025-03-27 23:40:30 -0700

    Specify initialization species fom wandb call in shell script

--- decima-main/scripts/submit_decima.sh
+++ decima-main/scripts/submit_decima.sh
@@ -3,8 +3,8 @@
 #SBATCH --partition=gpu            # Partition (queue) name
 #SBATCH --nodes=1                    # Request 4 nodes
 #SBATCH --ntasks=1                   # Request 4 tasks
-#SBATCH --gres=gpu:4                # Request 2 GPUs (of any type)
-#SBATCH --constraint="h100|h200"      # Only nodes with GPUs labeled h100 OR h200
+#SBATCH --gres=gpu:2                # Request 2 GPUs (of any type)
+#SBATCH --constraint="h100|h200|a100"      # Only nodes with GPUs labeled h100 OR h200
 #SBATCH --cpus-per-task=8          # Number of CPU cores per task (adjust as needed)
 #SBATCH --mem=100G                   # Total memory per node (adjust as needed)
 #SBATCH --time=12:00:00              # Time limit (2 days)
@@ -27,14 +27,14 @@ save_dir="/hpc/mydata/mathias.voges/Projects/research/seq2fun/step/data/"
 lr=3e-5
 weight=1e-4
 grad=5
-bs=2
+bs=4
 rep=0
 
 # Use SLURM_ARRAY_TASK_ID as the replicate number
-name="decima_v20250320_pretrained_rep${rep}_forecast_horizon_5_lstm_ae_ablateseq"
+name="decima_v20250327_pretrained-human_rep${rep}-smoothness-loss"
 
 # Run the fine-tuning script
-python ${scripts_dir}/finetune_temporal.py \
+python ${scripts_dir}/finetune.py \
     --name ${name} \
     --dir ${save_dir} \
     --lr ${lr} \
@@ -43,6 +43,6 @@ python ${scripts_dir}/finetune_temporal.py \
     --replicate ${rep} \
     --bs ${bs} \
     --init_mode pretrained \
-    --pretrained_source wandb \
+    --pretrained_source wandb-human \
     --wandb_project grelu/borzoi \
     --checkpoint_path None
